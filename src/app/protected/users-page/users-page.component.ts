f8101b67fff40fb9d1f6ce2b76fd6e84ece47730
aaron-baila <aaron.e.baila@gmail.com>
2022-10-05 03:39:30 +0200

    eliminar correcto

--- src/app/protected/users-page/users-page.component.ts
+++ src/app/protected/users-page/users-page.component.ts
@@ -28,14 +28,26 @@ export class UsersPageComponent {
 
     salert.fire({
       title: 'Seguro que quieres eliminar a ' + name,
+      icon: 'warning',
       showDenyButton: true,
       confirmButtonText: 'Eliminar',
       denyButtonText: `No eliminar`,
     }).then((result) => {
       if (result.isConfirmed) {
         this.usersService.eliminarUser(id);
-        salert.fire('Eliminado!', '', 'success');
+        // salert.fire('Eliminado!', name, 'success');
+        salert.fire({
+          position: 'top-end',
+          icon: 'success',
+          title: 'Eliminado correctamente',
+          showConfirmButton: false,
+          timer: 1500
+        })
       }
+      //El set time out es para que espere un segundo antes de recargar porque si no no datiempo a hacer la eliminacion
+      setTimeout(function () {
+        window.location.reload();
+      }, 1000);
     })
 
 
