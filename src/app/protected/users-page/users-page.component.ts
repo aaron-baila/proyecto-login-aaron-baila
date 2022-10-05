107ade7612f255eb071f2af9b94b380e18538649
aaron-baila <aaron.e.baila@gmail.com>
2022-10-05 03:02:15 +0200

    solo falta la peticion

--- src/app/protected/users-page/users-page.component.ts
+++ src/app/protected/users-page/users-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UsersService } from 'src/app/auth/services/users.service';
-
+import salert from 'sweetalert2';
 @Component({
   selector: 'app-users-page',
   templateUrl: './users-page.component.html',
@@ -20,6 +20,22 @@ export class UsersPageComponent {
     this.usersService.dameUsers().subscribe((datos) => {
       this.users = datos;
       console.log(this.users);
+    })
+  }
+
+  eliminar(id: string, name: string) {
+    console.log(id);
+
+    salert.fire({
+      title: 'Seguro que quieres eliminar a ' + name,
+      showDenyButton: true,
+      confirmButtonText: 'Eliminar',
+      denyButtonText: `No eliminar`,
+    }).then((result) => {
+      /* Read more about isConfirmed, isDenied below */
+      if (result.isConfirmed) {
+        salert.fire('Eliminado!', '', 'success')
+      }
     })
   }
 }
